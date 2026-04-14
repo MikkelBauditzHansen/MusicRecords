e2cb1baf4544ad74a8503e5b7358243f5a42a6b1
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2026-04-14 11:30:09 +0200

    sdfghj

--- script.js
+++ script.js
@@ -1,63 +1,111 @@
 const apiURL = "http://localhost:5151/api/music";
+const loginURL = "http://localhost:5151/api/auth/login";
+
 const app = Vue.createApp({
-    
     data() {
         return {
-            music:[],
+            music: [],
             searchTitle: "",
             searchArtist: "",
-            auth: { username: "", password: "" },
+            auth: {
+                username: "",
+                password: ""
+            },
             token: null,
-            role: null
-        }
+            role: null,
+            newMusic: {
+                title: "",
+                artist: "",
+                duration: 0,
+                year: 0
+            }
+        };
     },
     methods: {
-        loadMusic() {
-            axios.get(apiURL)
-                .then(response => {
-                    this.music = response.data;
+        addMusic() {
+            axios.post(apiURL, this.newMusic,{
+                headers: this.getAuthHeaders()
                 })
-                .catch(error => {
-                    console.error("Error fetching music data:", error);
-                    this.music = [];
-                });
+            .then(response => {
+                this.music.push(response.data);
+                this.newMusic.title = "";
+                this.newMusic.artist = "";
+                this.newMusic.duration = 0;
+                this.newMusic.year = 0;
+                alert("Music added successfully");
+            })
+            .catch(error => {
+                console.error("Error adding music:", error);
+                alert("Failed to add music");
+            });
         },
+        getAuthHeaders() {
+            if (this.token) {
+                return {
+                    Authorization: "Bearer " + this.token
+                };
+            }
+
+            return {};
+        },
+
+        loadMusic() {
+            axios.get(apiURL, {
+                headers: this.getAuthHeaders()
+            })
+            .then(response => {
+                this.music = response.data;
+            })
+            .catch(error => {
+                console.error("Error fetching music data:", error);
+                this.music = [];
+            });
+        },
+
         searchMusic() {
             axios.get(apiURL, {
                 params: {
                     title: this.searchTitle,
                     artist: this.searchArtist
-                }
+                },
+                headers: this.getAuthHeaders()
             })
-                .then(response => {
-                    this.music = response.data || [];
-                })
-                .catch(error => {
-                    console.error("Error searching music:", error);
-                    this.music = [];
+            .then(response => {
+                this.music = response.data || [];
+            })
+            .catch(error => {
+                console.error("Error searching music:", error);
+                this.music = [];
             });
         },
+
         clearSearch() {
-                 this.searchTitle = "";
-                 this.searchArtist = "";
-                  this.loadMusic();
+            this.searchTitle = "";
+            this.searchArtist = "";
+            this.loadMusic();
         },
+
         login() {
-            axios.post("http://localhost:5151/api/auth/login", this.auth)
-                .then(res => {
-                this.token = res.data.token;
-                this.role = res.data.role;
+            axios.post(loginURL, this.auth)
+            .then(response => {
+                this.token = response.data.token;
+                this.role = response.data.role;
                 this.loadMusic();
-        });
-}
-    },
-    mounted() {
-        this.loadMusic();
+            })
+            .catch(error => {
+                console.error("Login failed:", error);
+                alert("Login failed");
+            });
+        },
+
+        logout() {
+            this.token = null;
+            this.role = null;
+            this.music = [];
+            this.auth.username = "";
+            this.auth.password = "";
+        }
     }
 });
-axios.get(apiURL, {
-    headers: {
-        Authorization: "Bearer " + this.token
-    }
-})
+
 app.mount("#app");
